7ae2c6229c923b932d3feaef1c7b42d8effe252b
Ilya Kreymer <ikreymer@gmail.com>
2023-02-28 00:32:33 -0800

    new build: - update to latest wabac.js - use modified wabac.js to add COEP headers on all responses, to allow serving from fully static bucket - assets: add missing assets - rollup: add local dev and prod builds, dev build only uses remote for /images

--- src/native/owt-native.js
+++ src/native/owt-native.js
@@ -1,74 +1,23 @@
 import { LitElement, html, css } from 'lit-element';
 
-const ARCHIVE_PREFIX = __ARCHIVE_PREFIX__;
-
-const PROXY_PREFIX = __CORS_PREFIX__;
-
 
 export default class OWTNative extends LitElement
 {
-  constructor() {
-    super();
-    this.inited = false;
-  }
-
   static get properties() {
     return {
       url: { type: String },
       ts: { type: String },
       opts: { type: Object },
-      inited: { type: Boolean },
       iframeUrl: { type: String }
     }
-  }
-
-  firstUpdated() {
-    this.initSW();
   }
 
   updated(changedProps) {
     if (changedProps.has("url") || changedProps.has("ts")) {
       if (this.url && (this.url !== this.actualUrl || changedProps.has("ts"))) {
         this.dispatchEvent(new CustomEvent("load-started"));
-        this.iframeUrl = `/wabac/live/${this.ts}mp_/${this.url}`;
+        this.iframeUrl = `/w/live/${this.ts}mp_/${this.url}`;
       }
-    }
-  }
-
-  async initSW() {
-    const scope = "./";
-
-    await navigator.serviceWorker.register("./sw.js", {scope});
-
-    navigator.serviceWorker.addEventListener("message", (event) => {
-      this.inited = true;
-    });
-
-    const baseUrl = new URL(window.location);
-    baseUrl.hash = "";
-
-    const msg = {
-      msg_type: "addColl",
-      name: "live",
-      type: "live",
-      file: {"sourceUrl": `proxy:${PROXY_PREFIX}`},
-      skipExisting: false,
-      extraConfig: {
-        "prefix": PROXY_PREFIX, 
-        "isLive": false,
-        "archivePrefix": ARCHIVE_PREFIX,
-        "injectScripts": this.opts.injectScripts,
-        "baseUrl": baseUrl.href,
-        "baseUrlHashReplay": true
-      },
-    };
-
-    if (!navigator.serviceWorker.controller) {
-      navigator.serviceWorker.addEventListener("controllerchange", (event) => {
-        navigator.serviceWorker.controller.postMessage(msg);
-      });
-    } else {
-      navigator.serviceWorker.controller.postMessage(msg);
     }
   }
 
@@ -77,7 +26,7 @@ export default class OWTNative extends LitElement
   }
 
   render() {
-    if (!this.inited || !this.iframeUrl) {
+    if (!this.iframeUrl) {
       return html``;
     }
 
